68a630be254a06d9ce3c572fc319a4acc8e9be55
Nency2006 <nency417@gmail.com>
2026-04-06 11:56:49 +0530

    add frontend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ import 'dotenv/config';
 import connectDb from './database/db.js';
 const port = process.env.PORT || 8000;
 import userRoutes from './routes/userRoutes.js';
-
+import cors from 'cors';
 
 const app = express();
 connectDb();
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 app.use('/user', userRoutes);
 
